c69ec8538ad316d59a86cfe8593aecf970b2b7a5
zecorr <zecorr88@gmail.com>
2023-11-16 14:04:17 -0300

    Create power func

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ const subtract = (...num) => num.reduce((acc, curr) => acc - curr)
 const multiply = num => num.reduce((acc, curr) => acc * curr, 1)
 
 const divide = (...num) => num.reduce((acc, curr) => acc / curr)
+
+const power = (a, b) => Math.pow(a, b)
