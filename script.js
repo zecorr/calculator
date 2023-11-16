387b55f84664c50239e051a35109c18074428a89
zecorr <zecorr88@gmail.com>
2023-11-16 14:04:01 -0300

    Create divide func

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const add = (...num) => num.reduce((acc, curr) => acc + curr, 0)
 const subtract = (...num) => num.reduce((acc, curr) => acc - curr)
 
 const multiply = num => num.reduce((acc, curr) => acc * curr, 1)
+
+const divide = (...num) => num.reduce((acc, curr) => acc / curr)
