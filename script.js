3c749099e61bdb70fa565e2cc050af72c8dd2506
zecorr <zecorr88@gmail.com>
2023-11-16 14:03:38 -0300

    Create multiply func

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const add = (...num) => num.reduce((acc, curr) => acc + curr, 0)
 
 const subtract = (...num) => num.reduce((acc, curr) => acc - curr)
+
+const multiply = num => num.reduce((acc, curr) => acc * curr, 1)
