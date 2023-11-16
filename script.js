73480f86a5261adf6d0557fef593fa6685457585
zecorr <zecorr88@gmail.com>
2023-11-16 14:05:36 -0300

    Fix multiply func

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const add = (...num) => num.reduce((acc, curr) => acc + curr, 0)
 
 const subtract = (...num) => num.reduce((acc, curr) => acc - curr)
 
-const multiply = num => num.reduce((acc, curr) => acc * curr, 1)
+const multiply = (...num) => num.reduce((acc, curr) => acc * curr, 1)
 
 const divide = (...num) => num.reduce((acc, curr) => acc / curr)
 
